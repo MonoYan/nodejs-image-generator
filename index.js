8dcba115aac2f0ed29ea33333f253691a2b93497
Mono Yan <yanshencc@gmail.com>
2023-07-03 02:30:50 +0800

    backend

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 8000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended:false }))
+
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, ()=>console.log(`Server start on port ${port}`))
