aec49f639cfa5bd6b1bb8b93809617aa6c7f9bf7
Mono Yan <yanshencc@gmail.com>
2023-07-03 03:03:45 +0800

    frontend

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration)
 const generateImage = async(req, res)=>{
     const { prompt, size } = req.body
     const imageSize = size === 'small' ? '256x256' : size === 'medium' ?
-    '512x512' : '1024:1024'
+    '512x512' : '1024x1024'
 
     try {
         const response = await openai.createImage({
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 8000
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
